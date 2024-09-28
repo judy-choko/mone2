41c110b3d1b6195ae26e9c5435b7b7e609a23c13
judy-choko <114127806+judy-choko@users.noreply.github.com>
2024-09-28 15:34:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import io
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db.init_app(app)
