fcc671252f0ee4993871c9d8a93a1d4af27269b9
judy-choko <114127806+judy-choko@users.noreply.github.com>
2024-09-28 18:57:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from forms import RegistrationForm
 import io
 import matplotlib.pyplot as plt
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, SubmitField, PasswordField, DateField
+from wtforms import StringField, IntegerField, SelectField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
+from wtforms.fields.html5 import DateField
 
 app = Flask(__name__)
 
