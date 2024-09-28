56c2fe8ce385805030817353de5b0b55aed24b9b
judy-choko <114127806+judy-choko@users.noreply.github.com>
2024-09-28 15:42:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def expense_category_chart():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port,debug=True)
