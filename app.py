957032e68b55a2cd322338cf95467fd316786b9f
Yuka Nagae <chokokaruros@gmail.com>
2024-09-30 15:28:25 +0900

    fix app.py

--- app.py
+++ app.py
@@ -58,15 +58,9 @@ def get_fonts():
     
 def create_server_connection():
     # conn = psycopg2.connect(DATABASE_URL)
-    conn=psycopg2.connect(
-  database=DBNAME,
-  user=USERNAME,
-  host="/tmp",
-  password=PASSWORD
-)
     # conn = psycopg2.connect(dbname=DBNAME,host=LOCALHOST,port=5432,user=USERNAME,password=PASSWORD,sslmode="require")
     # conn = psycopg2.connect(dbname=DBNAME, user=USERNAME, password=PASSWORD)
-    # conn = psycopg2.connect(DBURL, sslmode='require')
+    conn = psycopg2.connect(DBURL, sslmode='require')
     # conn = psycopg2.connect(f'host={LOCALHOST} port=5432 dbname={DBNAME}  user={USERNAME} password={PASSWORD}')
     # connection = MySQLdb.connect(
     #     user=USERNAME, passwd=PASSWORD, host=LOCALHOST, db=DBNAME, charset="utf8"
