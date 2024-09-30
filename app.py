9c7df15cb66917089335ae0ddf54a8a30386b04d
Yuka Nagae <chokokaruros@gmail.com>
2024-09-30 15:54:01 +0900

    fix app.py

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ from flask import Flask, render_template_string
 # s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['PASSWORD'], os.environ['LOCALHOST'], os.environ['USERNAME'], os.environ['DBNAME'], os.environ['DATABASE_URL'], os.environ['DBURL'])
 # load_dotenv()
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-PASSWORD = os.environ.get("PASSWORD")
-LOCALHOST = os.environ.get("LOCALHOST")
-USERNAME = os.environ.get("USERNAME")
-DBNAME = os.environ.get("DBNAME")
-DATABASE_URL = os.environ.get('DATABASE_URL')
-DBURL = os.environ.get('DBURL')
+SECRET_KEY = os.environ['SECRET_KEY']
+PASSWORD = os.environ['PASSWORD']
+LOCALHOST = os.environ['LOCALHOST']
+USERNAME = os.environ['USERNAME']
+DBNAME = os.environ['DBNAME']
+DATABASE_URL = os.environ['DATABASE_URL']
+DBURL = os.environ['DBURL']
 # プッシュ
 
 app = Flask(__name__)
