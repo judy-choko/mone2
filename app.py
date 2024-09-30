2e1b6843bf01dd85687985b248782355c46a9996
judy-choko <114127806+judy-choko@users.noreply.github.com>
2024-09-30 12:16:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -585,7 +585,7 @@ def expense_category_chart():
     print(labels,values)
     try:
         fig, ax = plt.subplots(figsize=(2, 2))  # 幅5インチ、高さ3インチの画像
-        wedges, texts, autotexts = ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
+        wedges, texts, autotexts = ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90,, fontproperties=jp_font)
         ax.axis('equal')
         img = io.BytesIO()
         plt.savefig(img, format='png', dpi=150)  # dpi=300 で解像度を設定
