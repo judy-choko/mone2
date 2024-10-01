d571d666393612636494757464155e02bfc79e6a
Yuka Nagae <chokokaruros@gmail.com>
2024-10-01 14:13:07 +0900

    fix app.py

--- app/app.py
+++ app/app.py
@@ -5,15 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-GOOGLE_API_CREDS = os.environ.get("GOOGLE_API_CREDS")
-DBURL = os.environ.get("DBURL")
 TORIAEZU = os.environ.get("TORIAEZU")
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print(GOOGLE_API_CREDS)
-    return str(TORIAEZU)
+    return "hello"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
