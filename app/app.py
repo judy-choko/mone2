1198db82e4d93b3f5df4023d487ec0add24f6b7c
Yuka Nagae <chokokaruros@gmail.com>
2024-10-01 15:27:41 +0900

    fix app.py

--- app/app.py
+++ app/app.py
@@ -11,5 +11,6 @@ def index():
     return "hello"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
